71393335ac8677404d3422d7cbe90111ee55e829
Retro2D <longdaniel788@gmail.com>
2026-08-16 20:55:45 -0500

    Got rid of prototype code in __init__
    Got rid of prototype code in __init__

--- __init__.py
+++ __init__.py
@@ -8,15 +8,3 @@
 """
 
 __version__ = "0.0.1"
-
-import DanDB
-
-exampledata = []
-
-for i in range(16):
-    exampledata.append(i+1)
-
-mytable = DanDB.create_table("Example", 4, 4, exampledata)
-DanDB.pretty_print(mytable)
-
-mytable.writejson("example.json")
